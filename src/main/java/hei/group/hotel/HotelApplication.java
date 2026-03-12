eb47650a7827d9be7a870047051bead49e80fcca
nomena <hei.nomenjanahary@gmail.com>
2026-03-12 10:02:39 +0300

    first commit

---
+++ src/main/java/hei/group/hotel/HotelApplication.java
@@ -0,0 +1,13 @@
+package hei.group.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelApplication.class, args);
+    }
+
+}
