ad75821ce4b85c78c427abdfa57c2bbf4850244c
nomena <hei.nomenjanahary@gmail.com>
2026-03-13 18:23:22 +0300

    finition de la premiere question

--- src/main/java/hei/group/hotel/Controlleurs/Controlleur.java
+++ src/main/java/hei/group/hotel/Controlleurs/Controlleur.java
@@ -1,20 +1,22 @@
 package hei.group.hotel.Controlleurs;
-import hei.group.hotel.Entity.Reservation;
+
 import hei.group.hotel.Service.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Controlleur {
-    List<Reservation> reservations;
+    List<Service.Reservation> reservations = new ArrayList<>();
 
 
 
     @GetMapping("/booking")
-public List<Reservation> getBooking(){
+public List<Service.Reservation> getBooking(){
 return reservations;
     }
+
 }
